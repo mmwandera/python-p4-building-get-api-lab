8b6b220c62006016715d609399bc5e6044e3509e
mmwandera <mmwandera74@gmail.com>
2024-03-02 17:28:55 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
@@ -18,21 +16,35 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
+# GET /bakeries: returns an array of JSON objects for all bakeries in the database.
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return make_response(  bakeries,   200  )
 
+# GET /bakeries/<int:id>: returns a single bakery as JSON with its baked goods nested in an array. Use the id from the URL to look up the correct bakery.
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
 
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_serialized = bakery.to_dict()
+    return make_response ( bakery_serialized, 200  )
+
+# GET /baked_goods/by_price: returns an array of baked goods as JSON, sorted by price in descending order.
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
-
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_by_price_serialized = [
+        bg.to_dict() for bg in baked_goods_by_price
+    ]
+    return make_response( baked_goods_by_price_serialized, 200  )
+   
+# GET /baked_goods/most_expensive: returns the single most expensive baked good as JSON.
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    most_expensive_serialized = most_expensive.to_dict()
+    return make_response( most_expensive_serialized,   200  )
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
